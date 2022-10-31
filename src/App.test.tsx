308db294ae0af157b36d67ba19e2a95619b2ebdc
unknown <dibenvishvaa@gmail.com>
2022-11-01 06:34:05 +0800

    initial commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { Button, Input, Modal } from "antd";
+import { cleanup, fireEvent, render, screen } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import CSVReader from "./CsvParser/index";
+import React from "react";
+
+afterEach(cleanup);
+
+const defaultProps = {
+  onClick: jest.fn(),
+};
+
+describe("CSVReader", () => {
+  // test("button renders with correct text", () => {
+  //   const { queryByText, rerender } = render(<Button {...defaultProps} />);
+  //   expect(queryByText("Upload")).toHaveProperty("disabled", true);
+  // });
+
+  test("calls correct function on click", () => {
+    const onClick = jest.fn();
+    const { getByTestId } = render(
+      <Button data-testid="upload" onClick={onClick} />
+    );
+    fireEvent.click(getByTestId("upload"));
+    expect(onClick).toHaveBeenCalled();
+  });
+
+  test("should render csv file input element", async () => {
+    const { container, getByTestId } = render(<CSVReader data-testid="fileUpload" />);
+    fireEvent.click(getByTestId("fileUpload"));
+  });
 });
---
+++ src/CsvParser/index.js
@@ -0,0 +1,330 @@
+import "antd/dist/antd.css";
+
+import { Button, Input, Modal, Table, notification } from "antd";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import React, { CSSProperties, useEffect, useState } from "react";
+
+import { CSVReader } from "react-papaparse";
+import axios from "axios";
+
+export default function CSVParser() {
+  //STATE
+  const [isErr, setErr] = useState(false);
+  const [msg, setMsg] = useState("");
+  const [documentUploaded, setDocumentUpload] = useState(false);
+  const [globalCsv, setGlobalCSV] = useState();
+  const [isEditing, setIsEditing] = useState(false);
+  const [editingEmployee, setEditingEmployee] = useState(null);
+
+  const columns = [
+    {
+      key: "1",
+      title: "ID",
+      dataIndex: "id",
+      sorter: (record1, record2) => {
+        return record1.id > record2.id;
+      },
+    },
+    {
+      key: "2",
+      title: "Login",
+      dataIndex: "login",
+      sorter: (a, b) => a.login.length - b.login.length,
+    },
+    {
+      key: "3",
+      title: "Name",
+      dataIndex: "name",
+      sorter: (a, b) => a.name.length - b.name.length,
+    },
+    {
+      key: "4",
+      title: "Salary($)",
+      dataIndex: "salary",
+      sorter: (a, b) => a.salary - b.salary,
+    },
+    {
+      key: "5",
+      title: "Actions",
+      render: (record) => {
+        return (
+          <>
+            <EditOutlined
+              onClick={() => {
+                setIsEditing(true);
+                setEditingEmployee(record);
+              }}
+            />
+            <DeleteOutlined
+              onClick={() => {
+                deleteRequest(record);
+              }}
+              style={{ color: "red", marginLeft: 12 }}
+            />
+          </>
+        );
+      },
+    },
+  ];
+
+  //LOGIC
+  const checkDuplicate = (results) => {
+    const uniqueIDValues = new Set(results.map((v) => v.id));
+    const uniqueLoginValues = new Set(results.map((v) => v.login));
+    if (
+      uniqueIDValues.size < results.length ||
+      uniqueLoginValues.size < results.length
+    ) {
+      setErr(true);
+      setDocumentUpload(false)
+    } else {
+      setErr(false);
+      setMsg(
+        "New entry detected as shown. Proceed to upload to server for latest record"
+      );
+      setGlobalCSV(results);
+    }
+  };
+
+  const handleOnDrop = (data) => {
+    setDocumentUpload(true);
+    let content = data.map((result) => result.data);
+    let combinedContent = [...globalCsv || "", ...content]
+    checkDuplicate(combinedContent.flat());
+  };
+
+  const handleOnError = (err, file, inputElem, reason) => {
+    console.log(err);
+  };
+
+  const handleOnRemoveFile = (data) => {
+    setDocumentUpload(false);
+    setErr(false);
+  };
+
+  //API
+  const getEmployees = () => {
+    axios
+      .get("http://localhost:4000/employees")
+      .then((resp) => {
+        setGlobalCSV(resp.data.flat());
+      })
+      .catch(function (error) {
+        console.log(error);
+        if (error.code) {
+          notification.error({
+            message: error.name,
+            description: error.message,
+          });
+        }
+      });
+  };
+
+  const uploadEmployees = () => {
+    axios
+      .post("http://localhost:4000/employees", globalCsv)
+      .then(function (response) {
+        return response.data;
+      })
+      .then(() => getEmployees())
+      .catch(function (error) {
+        console.log(error);
+        if (error.code) {
+          notification.error({
+            message: error.name,
+            description: error.message,
+          });
+        }
+      });
+  };
+
+  // const updateEmployees = () => {
+  //   axios
+  //     .put("http://localhost:4000/employees", globalCsv)
+  //     .then(function (response) {
+  //       console.log(response);
+  //       return response.data;
+  //     })
+  //     .then(() => getEmployees())
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
+  // };
+
+  // const deleteAll = () => {
+  //   axios
+  //     .delete(`http://localhost:4000/employees/delete`)
+  //     .then((resp) => {
+  //       console.log("delete all resp", resp.data);
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
+  // };
+
+  // const getOneEmployee = () => {
+  //   axios
+  //     .get("http://localhost:4000/employees/")
+  //     .then((resp) => {
+  //       console.log(resp.data);
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
+  // };
+
+  const patchRequest = (record) => {
+    const update = {
+      id: record.id,
+      login: record.login,
+      name: record.name,
+    };
+    axios
+      .patch(`http://localhost:4000/employees/${record.id}`, update)
+      .then((resp) => {
+      })
+      .then(() => getEmployees())
+      .catch(function (error) {
+        console.log(error);
+        if (error.code) {
+          notification.error({
+            message: error.name,
+            description: error.message,
+          });
+        }
+      });
+  };
+
+  const deleteRequest = (record) => {
+    Modal.confirm({
+      title: "Are you sure, you want to delete this employee record?",
+      okText: "Yes",
+      okType: "danger",
+      onOk: () => {
+        axios
+          .delete(`http://localhost:4000/employees/${record.id}`)
+          .then((resp) => {
+          })
+          .then(() => getEmployees())
+          .catch(function (error) {
+            console.log(error);
+            if (error.code) {
+              notification.error({
+                message: error.name,
+                description: error.message,
+              });
+            }
+          });
+      },
+    });
+  };
+
+  const resetEditing = () => {
+    setIsEditing(false);
+    setEditingEmployee(null);
+  };
+
+  useEffect(() => {
+    getEmployees();
+  }, []);
+
+  useEffect(() => {
+  }, [globalCsv, documentUploaded]);
+
+  return (
+    <>
+      <h5>Click and Drag Upload</h5>
+      <CSVReader
+        onDrop={handleOnDrop}
+        onError={handleOnError}
+        addRemoveButton
+        onRemoveFile={handleOnRemoveFile}
+        config={{
+          header: true,
+          comments: true,
+          skipEmptyLines: true,
+          encoding: "ISO-8859-1",
+        }}
+        data-testid="fileUpload"
+      >
+        <span>Drop CSV file here or click to upload.</span>
+      </CSVReader>
+      {isErr && "duplicate id and login found"}
+      {msg} <br />
+      <Button
+        type="primary"
+        disabled={!documentUploaded}
+        onClick={() => uploadEmployees()}
+        data-testid="upload"
+      >
+        Upload
+      </Button>
+      <br />
+      <hr />
+      <br />
+      <Table
+        columns={columns}
+        dataSource={globalCsv}
+        rowKey="id"
+        pagination={{
+          defaultPageSize: 5,
+          showSizeChanger: true,
+          pageSizeOptions: ["5", "10", "15"],
+        }}
+      ></Table>
+      <Modal
+        title="Edit Employee"
+        visible={isEditing}
+        okText="Save"
+        onCancel={() => {
+          resetEditing();
+        }}
+        onOk={() => {
+          Modal.confirm({
+            title: "Are you sure, you want to edit this employee record?",
+            okText: "Yes",
+            okType: "danger",
+            onCancel: () => {
+              resetEditing();
+            },
+            onOk: () => {
+              patchRequest(editingEmployee);
+              resetEditing();
+            },
+          });
+        }}
+      >
+        <Input
+          value={editingEmployee?.name}
+          onChange={(e) => {
+            setEditingEmployee((pre) => {
+              return { ...pre, name: e.target.value };
+            });
+          }}
+        />
+        <Input
+          value={editingEmployee?.login}
+          onChange={(e) => {
+            setEditingEmployee((pre) => {
+              return { ...pre, login: e.target.value };
+            });
+          }}
+        />
+        <Input
+          value={editingEmployee?.salary}
+          onChange={(e) => {
+            let val = e.target.value;
+
+            if (!Number(val)) {
+              return;
+            }
+
+            setEditingEmployee((pre) => {
+              return { ...pre, salary: val };
+            });
+          }}
+        />
+      </Modal>
+    </>
+  );
+}
